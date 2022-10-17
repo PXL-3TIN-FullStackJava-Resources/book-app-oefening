a3b5527e649c37e0d123dde0af2871d3a16ec548
d-ries <swinnendries@gmail.com>
2022-10-17 09:32:37 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookItemComponent,
+    AddBookComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
